f4daedf56e87abc916db36b1c7a012ed99c3131f
bevzd <dbevzpr@mail.ru>
2024-12-03 21:24:24 +0200

    added api

--- backend/src/web/app/api/api.py
+++ backend/src/web/app/api/api.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
-from .endpoints import oauth2, teams
+from .endpoints import oauth2, teams, users, auth
 
 api = APIRouter()
 api.include_router(oauth2.r, prefix='/oauth', tags=['oauth'])
 api.include_router(teams.r, prefix='/teams', tags=['teams'])
+api.include_router(users.r, prefix='/users', tags=['users'])
+api.include_router(auth.r, prefix='/auth', tags=['auth'])
