91584a43cdcf6f0937fb13e0075ff7a2e14c7c5b
Nikita Gilevski <125219872+gilevski-tla-dev@users.noreply.github.com>
2024-12-03 18:43:06 +0200

    upd

--- frontend/src/app/router/AppRouter.tsx
+++ frontend/src/app/router/AppRouter.tsx
@@ -7,12 +7,14 @@ import {
 import { Layout } from "../layout";
 import { LoginPage } from "@/pages/LoginPage";
 import { RegistrationPage } from "@/pages/RegistrationPage";
+import { YandexLoading } from "@/pages/YandexLoading";
 
 export const AppRouter = () => {
   const routes = createRoutesFromElements(
     <Route path="/" element={<Layout />}>
       <Route path="/login" element={<LoginPage />} />
       <Route path="/registration" element={<RegistrationPage />} />
+      <Route path="/auth_loading" element={<YandexLoading />} />
     </Route>
   );
 
---
+++ frontend/src/pages/YandexLoading/index.ts
@@ -0,0 +1,1 @@
+export { YandexLoading } from "./YandexLoading";
