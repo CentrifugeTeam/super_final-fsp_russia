41fb2cf6254c251181474ea8c4cc8789aa6ebc5e
Nikita Gilevski <125219872+gilevski-tla-dev@users.noreply.github.com>
2024-12-04 21:46:18 +0200

    upd

--- frontend/src/app/providers/Providers.tsx
+++ frontend/src/app/providers/Providers.tsx
@@ -1,5 +1,7 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { FC } from "react";
+import { Provider as ReduxProvider } from "react-redux";
+import { store } from "@/app/redux/store";
 
 interface IProviders {
   children: JSX.Element;
@@ -9,9 +11,9 @@ const queryClient = new QueryClient();
 
 export const Providers: FC<IProviders> = ({ children }) => {
   return (
-    <>
+    <ReduxProvider store={store}>
       <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-    </>
+    </ReduxProvider>
   );
 };
 
