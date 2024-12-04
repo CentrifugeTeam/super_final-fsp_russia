f8847a1c98444917e8621afac9364c7c4a6c8fae
Nikita Gilevski <125219872+gilevski-tla-dev@users.noreply.github.com>
2024-12-04 03:55:36 +0200

    Update AppRouter.tsx

--- frontend/src/app/router/AppRouter.tsx
+++ frontend/src/app/router/AppRouter.tsx
@@ -3,7 +3,6 @@ import {
   createRoutesFromElements,
   Route,
   RouterProvider,
-  Navigate,
 } from "react-router-dom";
 import { Layout } from "../layout";
 import { LoginPage } from "@/pages/LoginPage";
@@ -13,14 +12,9 @@ import { SendCode } from "@/pages/SendCode";
 export const AppRouter = () => {
   const routes = createRoutesFromElements(
     <Route path="/" element={<Layout />}>
-      {/* Если пользователь на корневом маршруте, перенаправляем на /registration */}
-      <Route index element={<Navigate to="/registration" replace />} />
-      {/* Другие маршруты */}
       <Route path="/login" element={<LoginPage />} />
       <Route path="/registration" element={<RegistrationPage />} />
       <Route path="/auth_loading/:code" element={<SendCode />} />
-      {/* Если путь не определен, перенаправляем на /registration */}
-      <Route path="*" element={<Navigate to="/registration" replace />} />
     </Route>
   );
 
