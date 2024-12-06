04ab2f67324090763bb6c8f3f65007a62301cf05
Nikita Gilevski <125219872+gilevski-tla-dev@users.noreply.github.com>
2024-12-06 12:33:33 +0200

    upd

--- frontend/platform/src/components/Header/ui/Header.tsx
+++ frontend/platform/src/components/Header/ui/Header.tsx
@@ -1,16 +1,30 @@
+import { useState } from "react";
 import styles from "./header.module.scss";
 import Logo from "@/assets/logo_fsp.svg";
+import MenuIcon from "@/assets/burger_button.svg"; // Импорт SVG для кнопки
 
 export const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <div className={styles.header}>
-      <div>
-        <img src={Logo} alt="" />
+      <div className={styles.logoMenu}>
+        <img src={Logo} alt="Логотип" className={styles.logo} />
+        <img
+          src={MenuIcon}
+          alt="Меню"
+          className={styles.menuIcon}
+          onClick={toggleMenu}
+        />
       </div>
-      <div className={styles.links}>
+      <div className={`${styles.links} ${isMenuOpen ? styles.open : ""}`}>
         <a>О нас</a>
         <a>Регионы</a>
-        <a> Календарь</a>
+        <a>Календарь</a>
         <a>Статистика</a>
         <a>Контакты</a>
         <a>Личный кабинет</a>
