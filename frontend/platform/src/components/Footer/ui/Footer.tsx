ef2287abe47a60e88e60ee11e7fb44972ccd80c5
Nikita Gilevski <125219872+gilevski-tla-dev@users.noreply.github.com>
2024-12-06 13:57:12 +0200

    upd

--- frontend/platform/src/components/Footer/ui/Footer.tsx
+++ frontend/platform/src/components/Footer/ui/Footer.tsx
@@ -1,5 +1,7 @@
 import styles from "./footer.module.scss";
 import Logo from "@/assets/logo_fsp.svg";
+import VkQr from "@/assets/qr_vk.svg";
+import TgQr from "@/assets/tg_qr.svg";
 
 export const Footer = () => {
   return (
@@ -21,8 +23,14 @@ export const Footer = () => {
         <h2>press@fsp-russia.com</h2>
       </div>
       <div>
-        <img src="" alt="" />
-        <img src="" alt="" />
+        <img src={VkQr} alt="" />
+        <img src={TgQr} alt="" />
+      </div>
+      <div>
+        <h1>Контакты</h1>
+        <h2>info@fsp-russia.com</h2>
+        <h2>+7 (499) 678 03 05</h2>
+        <h2>125047, г. Москва, 2-я Брестская, д.8, этаж 9</h2>
       </div>
     </div>
   );
