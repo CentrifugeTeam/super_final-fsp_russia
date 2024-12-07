98bc28b9d1084850d708e824061c1745e6eb952b
GermaMirn <144611146+GermaMirn@users.noreply.github.com>
2024-12-07 13:19:27 +0200

    a

--- frontend/platform/src/features/EditProfile/ProfileEdit.tsx
+++ frontend/platform/src/features/EditProfile/ProfileEdit.tsx
@@ -1,22 +1,30 @@
 import styles from "./profileedit.module.scss";
 import { Button } from "@/components/ui/button";
 import { ProfileEditCard } from "@/components/ProfileEditCard";
+import { ProfileEditChange } from "../ProfileEditChange";
 
 export const ProfileEdit = () => {
+	const isEdit = true
+
 	return (
 		<div className={styles.contet}>
 			<div className={styles.header}>
 				<h1 className={styles.headerText}>Мой профиль</h1>
 
 				<Button
-					className="h-[50px] bg-[#463ACB] text-[20px] mt-5"
+					className="h-[50px] bg-[#463ACB] text-[20px] mt-4"
 				>
 					Редактировать профиль
 				</Button>
 			</div>
 
 			<div className={styles.profileEditComponenst}>
-				<ProfileEditCard />
+				{
+					isEdit ?
+						<ProfileEditChange />
+					:
+						<ProfileEditCard />
+				}
 			</div>
 		</div>
 	);
