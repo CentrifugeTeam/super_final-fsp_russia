56c577361b7127a6bf9b5a6d42e96476b0309ca5
Nikita Gilevski <125219872+gilevski-tla-dev@users.noreply.github.com>
2024-12-07 17:25:19 +0200

    upd

--- frontend/platform/src/app/router/AppRouter.tsx
+++ frontend/platform/src/app/router/AppRouter.tsx
@@ -12,16 +12,19 @@ import { SendCode } from "@/pages/SendCode";
 import { ProtectedRoute } from "@/features/ProtectedRoute";
 import { ProfilePage } from "@/pages/ProfilePage";
 import SendCodeVk from "@/pages/SendCodeVk/SendCodeVk";
-// import { FeedPage } from "@/pages/FeedPage";
 import { SendEmail } from "@/pages/SendEmail";
 import { SendPassword } from "@/pages/SendPassword";
 import { RegionByIdPage } from "@/pages/RegionByIdPage";
-import { ContactsPage } from "@/pages/ContactsPage";
+import { ProfileEdit } from "@/features/EditProfile";
+import { RequestEdit } from "@/features/RequestEdit";
+import { SolutionEdit } from "@/features/SolutionEdit";
+import { RatingEdit } from "@/features/RatingEdit";
+import { NewRequest } from "@/pages/NewRequest";
 
 export const AppRouter = () => {
   const routes = createRoutesFromElements(
     <Route path="/" element={<Layout />}>
-      {/* Redirect to /about_us by default */}
+      {/* Redirect to /regions by default */}
       <Route index element={<Navigate to="/regions" replace />} />
 
       {/* Открытые страницы */}
@@ -29,17 +32,20 @@ export const AppRouter = () => {
       <Route path="/registration" element={<RegistrationPage />} />
       <Route path="/auth_loading" element={<SendCode />} />
       <Route path="/auth_loading_vk" element={<SendCodeVk />} />
-      {/* <Route path="/regions" element={<FeedPage />} /> */}
       <Route path="/send_email" element={<SendEmail />} />
       <Route path="/send_password" element={<SendPassword />} />
-      <Route path="/regions/region/:id" element={<RegionByIdPage />} />
-			<Route path="/contacts" element={<ContactsPage />} />
-
+      <Route path="/regions" element={<RegionByIdPage />} />
 
       {/* Защищенные страницы */}
       <Route element={<ProtectedRoute />}>
-        <Route path="/profile" element={<ProfilePage />} />
-        <Route path="/profile/*" element={<ProfilePage />} />
+        <Route path="/profile" element={<ProfilePage />}>
+          {/* Вложенные маршруты для различных вкладок профиля */}
+          <Route path="edit" element={<ProfileEdit />} />
+          <Route path="requests" element={<RequestEdit />} />
+          <Route path="requests/new" element={<NewRequest />} />
+          <Route path="solutions" element={<SolutionEdit />} />
+          <Route path="rating" element={<RatingEdit />} />
+        </Route>
       </Route>
     </Route>
   );
--- frontend/platform/src/pages/ProfilePage/ProfilePage.tsx
+++ frontend/platform/src/pages/ProfilePage/ProfilePage.tsx
@@ -1,11 +1,11 @@
-import { useLocation } from "react-router-dom";
-import { ProfileEdit } from "@/features/EditProfile";
+import { Outlet, useLocation } from "react-router-dom";
+// import { ProfileEdit } from "@/features/EditProfile";
 import { ProfilePanel } from "@/features/ProfilePanel";
 import style from "./profilepage.module.scss";
-import { RequestEdit } from "@/features/RequestEdit";
-import { SolutionEdit } from "@/features/SolutionEdit"; // Импортируем компонент SolutionEdit
-import { RatingEdit } from "@/features/RatingEdit";
-import { NewRequest } from "../NewRequest";
+// import { RequestEdit } from "@/features/RequestEdit";
+// import { SolutionEdit } from "@/features/SolutionEdit"; // Импортируем компонент SolutionEdit
+// import { RatingEdit } from "@/features/RatingEdit";
+// import { NewRequest } from "../NewRequest";
 
 export const ProfilePage = () => {
   const location = useLocation();
@@ -19,20 +19,7 @@ export const ProfilePage = () => {
       </div>
 
       <div className={style.content}>
-        {/* Условный рендеринг на основе последнего сегмента пути */}
-        {activeComponent === "edit" ? (
-          <ProfileEdit />
-        ) : activeComponent === "requests" ? (
-          <RequestEdit />
-        ) : activeComponent === "solutions" ? ( // Добавляем условие для solutions
-          <SolutionEdit />
-        ) : activeComponent === "rating" ? ( // Добавляем условие для solutions
-          <RatingEdit />
-        ) : activeComponent === "new_req" ? ( // Добавляем условие для solutions
-          <NewRequest />
-        ) : (
-          <p>Добро пожаловать в ваш профиль!</p>
-        )}
+        <Outlet />
       </div>
     </div>
   );
