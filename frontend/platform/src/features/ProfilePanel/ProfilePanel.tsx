d11f0a40ebed821ec4ab9707c74a22a6cdab4eec
Nikita Gilevski <125219872+gilevski-tla-dev@users.noreply.github.com>
2024-12-07 12:56:06 +0200

    upd

--- frontend/platform/src/features/ProfilePanel/ProfilePanel.tsx
+++ frontend/platform/src/features/ProfilePanel/ProfilePanel.tsx
@@ -1,25 +1,45 @@
+import { useNavigate, useLocation } from "react-router-dom";
 import styles from "./profilepanel.module.scss";
 import { RoleProfilePanel } from "@/components/RoleProfilePanel";
 import { PersonInfoProfilePanel } from "@/components/PersonInfoProfilePanel";
 
 export const ProfilePanel = () => {
-	return (
-		<div className={styles.profilePanel}>
-			<div className={styles.role}>
-				<RoleProfilePanel />
-			</div>
+  const navigate = useNavigate();
+  const location = useLocation(); // Определение текущего пути
 
-			<div className={styles.personInfo}>
-				<PersonInfoProfilePanel />
-			</div>
-			<hr className={styles.hr} />
+  return (
+    <div className={styles.profilePanel}>
+      <div className={styles.role}>
+        <RoleProfilePanel />
+      </div>
 
-			<div className={styles.actives}>
-				<h3 className={styles.active}>Мой профиль</h3>
-				<h3 className={styles.active}>Заявки</h3>
-			</div>
-		</div>
-	);
+      <div className={styles.personInfo}>
+        <PersonInfoProfilePanel />
+      </div>
+      <hr className={styles.hr} />
+
+      <div className={styles.actives}>
+        <h3
+          className={`${styles.active} ${
+            location.pathname === "/profile/edit" ? styles.activeSelected : ""
+          }`}
+          onClick={() => navigate("/profile/edit")}
+        >
+          Мой профиль
+        </h3>
+        <h3
+          className={`${styles.active} ${
+            location.pathname === "/profile/requests"
+              ? styles.activeSelected
+              : ""
+          }`}
+          onClick={() => navigate("/profile/requests")}
+        >
+          Заявки
+        </h3>
+      </div>
+    </div>
+  );
 };
 
 export default ProfilePanel;
--- frontend/platform/src/features/RequestEdit/RequestEdit.tsx
+++ frontend/platform/src/features/RequestEdit/RequestEdit.tsx
@@ -1,21 +1,22 @@
-import styles from "./profileedit.module.scss";
+import RequestEditCard from "@/components/RequestEditCard/RequestEditCard";
+import styles from "./requestedit.module.scss";
 import { Button } from "@/components/ui/button";
-import { ProfileEditCard } from "@/components/ProfileEditCard";
 
-export const ProfileEdit = () => {
+export const RequestEdit = () => {
   return (
     <div className={styles.contet}>
       <div className={styles.header}>
-        <h1 className={styles.headerText}>Мой профиль</h1>
-
+        <h1 className={styles.headerText}>Заявки</h1>
         <Button className="h-[50px] bg-[#463ACB] text-[20px] mt-5">
-          Редактировать профиль
+          Новая заявка
         </Button>
       </div>
 
       <div className={styles.profileEditComponenst}>
-        <ProfileEditCard />
+        <RequestEditCard />
       </div>
     </div>
   );
 };
+
+export default RequestEdit;
