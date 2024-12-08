af7b581b2a9b95696942341f637b7ecfbde90c1e
Nikita Gilevski <125219872+gilevski-tla-dev@users.noreply.github.com>
2024-12-08 07:36:43 +0200

    upd

--- frontend/platform/src/components/SolutionEditCard/SoultionEditCard.tsx
+++ frontend/platform/src/components/SolutionEditCard/SoultionEditCard.tsx
@@ -1,6 +1,17 @@
+import { useTeams } from "@/shared/api/getTeams"; // Хук для запроса данных
 import styles from "./solutioneditcard.module.scss";
 
-export const SolutionEditCard = () => {
+interface SolutionEditCardProps {
+  selectedRegion: string; // Пропс для выбранного региона
+}
+
+export const SolutionEditCard = ({ selectedRegion }: SolutionEditCardProps) => {
+  const { data, isLoading, isError } = useTeams({
+    federal_name: selectedRegion === "all" ? "" : selectedRegion, // Если "Все регионы", то параметр пустой
+    page: 1,
+    size: 50,
+  });
+
   return (
     <div className={styles.content}>
       <div className={styles.table}>
@@ -9,11 +20,15 @@ export const SolutionEditCard = () => {
         <h1>Рейтинг</h1>
       </div>
 
-      <div className={styles.table2}>
-        <h1>Команды</h1>
-        <h1>Регион</h1>
-        <h1>Рейтинг</h1>
-      </div>
+      {isLoading && <p>Загрузка...</p>}
+      {isError && <p>Ошибка загрузки данных</p>}
+      {data?.items.map((team) => (
+        <div className={styles.table2}>
+          <h1>{team.name}</h1>
+          <h1>{team.federal.name}</h1>
+          <h1>Оценить</h1>
+        </div>
+      ))}
     </div>
   );
 };
